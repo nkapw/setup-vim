f03fb8e672c5090d3e379a86c1d5ebdd21174fb8
Imam A. Fahrezi <imamfahrezi24@gmail.com>
2025-03-03 04:26:49 +0700

    update bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,5 +3,24 @@ return {
 		"akinsho/bufferline.nvim",
 		version = "*",
 		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					indicator = {
+						-- style = "underline",
+					},
+					separator_style = "slant",
+					diagnostics = "nvim_lsp",
+					diagnostics_indicator = function(count, level, diagnostics_dict, context)
+						local icon = level:match("error") and " " or " "
+						if context.buffer:current() then
+							return "" .. icon .. count
+						end
+
+						return "" .. icon .. count
+					end,
+				},
+			})
+		end,
 	},
 }

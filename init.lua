bcd21c575fbc06efaba909d284133270dc26786e
Imam A. Fahrezi <imamfahrezi24@gmail.com>
2026-05-30 22:46:47 +0700

    migrasi to vim.pack

--- init.lua
+++ init.lua
@@ -1,2 +1,403 @@
-require("config.lazy")
+-- INFO: introduction
+-- this is a minimal neovim configuration written in lua. this is not meant to
+-- be a distribution, but rather a template for you to build upon and/or a
+-- reference for how to configure neovim using lua in the latest version.
+--
+-- TUTOR:
+-- if you're completely new to neovim and/or vim, consider going through
+-- `:Tutor` inside neovim to get a basic idea of how it works.
+--     if you don't know what this means, type the following:
+--       - <escape key>
+--       - :
+--       - Tutor
+--       - <enter key>
+--
+-- LUA:
+-- some level of familiarity with lua/programming languages are also expected.
+-- if you're new to lua, consider going through the official reference:
+--    https://www.lua.org/manual
+-- or a more friendly tutorial like:
+--    https://learnxinyminutes.com/docs/lua/
+-- you can also check out `:h lua-guide` inside neovim for a neovim-specific
+-- lua guide.
+--
+-- DEPENDENCIES:
+-- this configuration assumes you have the following tools installed on your
+-- system:
+--    `git` - for vim builtin package manager. (see `:h vim.pack`)
+--    `ripgrep` - for fuzzy finding
+--    clipboard tool: xclip/xsel/win32yank - for clipboard sharing between OS and neovim (see `h: clipboard-tool`)
+--    a nerdfont (ensure the terminal running neovim is using it)
+-- run `:checkhealth` inside neovim to see if your system is missing anything.
+--
+-- MINIMAL:
+-- to say that something is 'minimal' you have to define what variable you're
+-- minimizing. this configuration minimizes for lines of code and concepts.
+-- to some, this configuration may have too many plugins. for example, using
+-- mason.nvim to manage lsp servers will be an unnecessary dependency if the
+-- user is already familiar with lsps and is comfortable managing them through
+-- their OS package manager. but to someone that isn't familiar with lsp servers
+-- this approach wouldn't cover everything needed to have the 'minimum' necessary
+-- for lsp + completion + fuzzy finding. to some, fuzzy finding is also a bloated
+-- dependency.
+-- this configuration is only a starting point/reference. it is expected that
+-- the user will change the configuration to suit their needs.
 
+-- INFO: options
+-- these change the default neovim behaviours using the 'vim.opt' API.
+-- see `:h vim.opt` for more details.
+-- run `:h '{option_name}'` to see what they do and what values they can take.
+-- for example, `:h 'number'` for `vim.opt.number`.
+
+-- set <space> as the leader key
+-- must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- enable true color support
+vim.opt.termguicolors = true
+
+-- make line numbers default
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
+
+-- don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- sync clipboard between OS and Neovim.
+--  remove this option if you want your OS clipboard to remain independent.
+--  see `:help 'clipboard'`
+vim.opt.clipboard = "unnamedplus"
+
+-- enable break indent
+vim.opt.breakindent = true
+
+-- save undo history
+vim.opt.undofile = true
+
+-- case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- decrease update time
+vim.opt.updatetime = 250
+
+-- decrease mapped sequence wait time
+-- displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- show which line your cursor is on
+vim.opt.cursorline = true
+
+-- set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+
+-- enable line wrapping
+vim.opt.wrap = true
+
+-- formatting
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.textwidth = 80
+
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+		},
+	},
+	virtual_text = true, -- show inline diagnostics
+})
+
+-- clear search highlights with <Esc>
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- INFO: plugins
+-- we install plugins with neovim's builtin package manager: vim.pack
+-- and then enable/configure them by calling their setup functions.
+--
+-- (see `:h vim.pack` for more details on how it works)
+-- you can press `gx` on any of the plugin urls below to open them in your
+-- browser and check out their documentation and functionality.
+-- alternatively, you can run `:h {plugin-name}` to read their documentation.
+--
+-- plugins are then loaded and configured with a call to `setup` functions
+-- provided by each plugin. this is not a rule of neovim but rather a convention
+-- followed by the community.
+-- these setup calls take a table as an agument and their expected contents can
+-- vary wildly. refer to each plugin's documentation for details.
+
+-- INFO: colorscheme
+vim.cmd.colorscheme("catppuccin")
+
+-- INFO: formatting and syntax highlighting
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" }, { confirm = false })
+
+-- equivalent to :TSUpdate
+require("nvim-treesitter.install").update("all")
+
+require("nvim-treesitter").setup({
+	sync_install = true,
+
+	modules = {},
+	ignore_install = {},
+
+	ensure_installed = {
+		"lua",
+		"c",
+		"rust",
+		"go",
+	},
+
+	auto_install = true, -- autoinstall languages that are not installed yet
+
+	highlight = {
+		enable = true,
+	},
+})
+
+-- INFO: completion engine
+vim.pack.add({ "https://github.com/saghen/blink.cmp" }, { confirm = false })
+
+require("blink.cmp").setup({
+	completion = {
+		documentation = {
+			auto_show = true,
+		},
+	},
+
+	keymap = {
+		-- these are the default blink keymaps
+		["<C-n>"] = { "select_next", "fallback_to_mappings" },
+		["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+		["<C-y>"] = { "select_and_accept", "fallback" },
+		["<C-e>"] = { "cancel", "fallback" },
+
+		["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+		["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+		["<CR>"] = { "select_and_accept", "fallback" },
+		["<Esc>"] = { "cancel", "hide_documentation", "fallback" },
+
+		["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+
+		["<C-b>"] = { "scroll_documentation_up", "fallback" },
+		["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+		["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+	},
+
+	fuzzy = {
+		implementation = "lua",
+	},
+})
+
+-- INFO: lsp server installation and configuration
+
+-- lsp servers we want to use and their configuration
+-- see `:h lspconfig-all` for available servers and their settings
+local lsp_servers = {
+	lua_ls = {
+		-- https://luals.github.io/wiki/settings/ | `:h nvim_get_runtime_file`
+		Lua = { workspace = { library = vim.api.nvim_get_runtime_file("lua", true) } },
+	},
+	gopls = {
+		cmd = { "gopls" },
+		filetypes = { "go", "gomod", "gowork", "gotmpl" },
+		root_markers = { "go.work", "go.mod", ".git" },
+		settings = {
+			gopls = {
+				gofumpt = true,
+				staticcheck = true,
+				hints = {
+					assignVariableTypes = true,
+					compositeLiteralFields = true,
+					compositeLiteralTypes = true,
+					constantValues = true,
+					functionTypeParameters = true,
+					parameterNames = true,
+					rangeVariableTypes = true,
+				},
+			},
+		},
+	},
+}
+
+vim.pack.add({
+	"https://github.com/neovim/nvim-lspconfig", -- default configs for lsps
+
+	-- NOTE: if you'd rather install the lsps through your OS package manager you
+	-- can delete the next three mason-related lines and their setup calls below.
+	-- see `:h lsp-quickstart` for more details.
+	"https://github.com/mason-org/mason.nvim", -- package manager
+	"https://github.com/mason-org/mason-lspconfig.nvim", -- lspconfig bridge
+	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim", -- auto installer
+}, { confirm = false })
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-tool-installer").setup({
+	ensure_installed = vim.tbl_keys(lsp_servers),
+})
+
+-- configure each lsp server on the table
+-- to check what clients are attached to the current buffer, use
+-- `:checkhealth vim.lsp`. to view default lsp keybindings, use `:h lsp-defaults`.
+local function on_attach(_, bufnr)
+	local map = vim.keymap.set
+	local opts = function(desc)
+		return { buffer = bufnr, desc = desc }
+	end
+
+	map("n", "gd", vim.lsp.buf.definition, opts("LSP: [G]o to [D]efinition"))
+	map("n", "gD", vim.lsp.buf.declaration, opts("LSP: [G]o to [D]eclaration"))
+	map("n", "gr", vim.lsp.buf.references, opts("LSP: [G]o to [R]eferences"))
+	map("n", "gi", vim.lsp.buf.implementation, opts("LSP: [G]o to [I]mplementation"))
+	map("n", "gf", vim.lsp.buf.format, opts("LSP: [G]o [F]ormat"))
+	map("n", "K", vim.lsp.buf.hover, opts("LSP: Hover Documentation"))
+	map("n", "<leader>rn", vim.lsp.buf.rename, opts("LSP: [R]e[n]ame"))
+	map("n", "<leader>ca", vim.lsp.buf.code_action, opts("LSP: [C]ode [A]ction"))
+	map("n", "<leader>ld", vim.diagnostic.open_float, opts("LSP: [L]ine [D]iagnostic"))
+	map("n", "[d", function()
+		vim.diagnostic.jump({ count = -1 })
+	end, opts("LSP: Previous [D]iagnostic"))
+	map("n", "]d", function()
+		vim.diagnostic.jump({ count = 1 })
+	end, opts("LSP: Next [D]iagnostic"))
+
+	-- bonus: langsung jump ke error (bukan warning)
+	map("n", "[e", function()
+		vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR })
+	end, opts("LSP: Previous [E]rror"))
+	map("n", "]e", function()
+		vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR })
+	end, opts("LSP: Next [E]rror"))
+end
+
+for server, config in pairs(lsp_servers) do
+	vim.lsp.config(server, {
+		settings = config,
+		on_attach = on_attach,
+	})
+	vim.lsp.enable(server)
+end
+
+-- NOTE: if all you want is lsp + completion + highlighting, you're done.
+-- the rest of the lines are just quality-of-life/appearance plugins and
+-- can be removed.
+
+-- INFO: fuzzy finder
+vim.pack.add({
+	"https://github.com/nvim-lua/plenary.nvim", -- library dependency
+	"https://github.com/nvim-tree/nvim-web-devicons", -- icons (nerd font)
+	"https://github.com/nvim-telescope/telescope.nvim", -- the fuzzy finder
+}, { confirm = false })
+
+require("telescope").setup({})
+
+local pickers = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>fp", pickers.builtin, { desc = "[S]earch Builtin [P]ickers" })
+vim.keymap.set("n", "<leader>fb", pickers.buffers, { desc = "[S]earch [B]uffers" })
+vim.keymap.set("n", "<leader>ff", pickers.find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>fw", pickers.grep_string, { desc = "[S]earch Current [W]ord" })
+vim.keymap.set("n", "<leader>fg", pickers.live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>fr", pickers.resume, { desc = "[S]earch [R]esume" })
+
+vim.keymap.set("n", "<leader>fh", pickers.help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>fm", pickers.man_pages, { desc = "[S]earch [M]anuals" })
+
+-- INFO: better statusline
+vim.pack.add({ "https://github.com/nvim-lualine/lualine.nvim" }, { confirm = false })
+
+require("lualine").setup({
+	options = {
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
+	},
+})
+
+-- INFO: keybinding helper
+vim.pack.add({ "https://github.com/folke/which-key.nvim" }, { confirm = false })
+
+require("which-key").setup({
+	spec = {
+		{ "<leader>s", group = "[S]earch", icon = { icon = "", color = "green" } },
+	},
+})
+
+-- NOTE: there are many more quality-of-life plugins available and others that
+-- achieve what these do. these are just our recommendations to start.
+
+-- INFO: utility plugins
+vim.pack.add({
+	"https://github.com/windwp/nvim-autopairs", -- auto pairs
+	"https://github.com/folke/todo-comments.nvim", -- highlight TODO/INFO/WARN comments
+}, { confirm = false })
+
+require("nvim-autopairs").setup()
+require("todo-comments").setup()
+
+-- INFO nvim-tree
+vim.pack.add({
+	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
+})
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+require("nvim-tree").setup({
+	view = {
+		adaptive_size = true,
+	},
+	update_focused_file = {
+		enable = true,
+	},
+	sync_root_with_cwd = true,
+	respect_buf_cwd = true,
+})
+
+-- INFO conform for format on save
+vim.pack.add({
+	{ src = "https://github.com/stevearc/conform.nvim" },
+})
+require("conform").setup({
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		json = { "jq" },
+		rust = { "rustfmt" },
+		python = { "black" },
+		go = { "goimports", "gofumpt" },
+		html = { "prettier" },
+		javascript = { "prettier" },
+	},
+})
+
+-- uncomment to enable automatic plugin updates
+-- vim.pack.update()
